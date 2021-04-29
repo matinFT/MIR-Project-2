f2f0d358db91b2e93189c47d13801691710e4729
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2021-04-29 16:06:49 +0430

    added bernoulli state for NaiveBayes

--- src/classification/models/nb.py
+++ src/classification/models/nb.py
@@ -12,12 +12,26 @@ class NaiveBayes(BaseEstimator, ClassifierMixin):
 
 #     def fit(self, x, y, **fit_params):
         
-    def fit(self, x, y):
+    def fit(self, X, Y):
+        self.classes_num = max(Y)
+        self.class_priors = self.cal_class_priors(Y)
         if self.kind == "gaussian":
-            self.classes_num = max(y)
-            self.class_priors = self.cal_class_priors(y)
-            self.means, self.stds = self.cal_means_variances(x, y)
-    
+            self.means, self.stds = self.cal_means_variances(X, Y)
+        else:
+            self.thresholds, self.conditional_probs = self.cal_thresholds_conditional_probs(X, Y)
+            
+    def cal_thresholds_conditional_probs(self, X, Y):
+        conditional_probs = np.zeros([len(X[0]), self.classes_num + 1])
+        total_class = np.zeros([len(X[0]), self.classes_num + 1])
+        positive_class = np.zeros([len(X[0]), self.classes_num + 1])
+        thresholds = sum(np.array([np.array(x) for x in X])) / len(X[0])
+        for i in range(len(X)):
+            for j in range(len(X[0])):
+                total_class[j, Y[i]] += 1
+                if (X[i][j] > thresholds[j]):
+                    positive_class[j][Y[i]] += 1
+        return thresholds, positive_class / total_class
+                
     def cal_means_variances(self, X, Y): # calculates means of gaussian p(t|c)
         means = np.zeros([len(X[0]), self.classes_num + 1])
         stds = np.zeros([len(X[0]), self.classes_num + 1])
@@ -42,10 +56,10 @@ class NaiveBayes(BaseEstimator, ClassifierMixin):
         if self.kind == "gaussian":
             return self.pred_gaussian(X)
         else:
-            return pred_bernouli(X)
+            return self.pred_bernouli(X)
     
     def pred_gaussian(self, X):
-        Y = []
+        pred = []
         constant = 1 / np.sqrt(2 * np.pi)
         for x in X:
             class_probs = []
@@ -55,9 +69,27 @@ class NaiveBayes(BaseEstimator, ClassifierMixin):
                 for p in term_probs:
                     prob += np.log(p)
                 class_probs.append(prob)
-            Y.append(np.argmax(class_probs))
-        return Y
+            pred.append(np.argmax(class_probs))
+        return pred
     
     def pred_bernouli(self, X):
-        pass
-                                                                                
+        pred = []
+        for x in X:
+            x_bernoulli = [0] * len(x)
+            for i in range(len(x)):
+                if x[i] >= self.thresholds[i]:
+                    x_bernoulli[i] = 1
+                    
+            class_probs = []
+            for c in range(self.classes_num + 1):
+                term_probs = [self.conditional_probs[i][c] if x_bernoulli[i] == 1 else 1 - self.conditional_probs[i][c] for i in range(len(x_bernoulli)) ]
+                prob = np.log(self.class_priors[c])
+                for p in term_probs:
+                    prob += np.log(p)
+                class_probs.append(prob)
+            pred.append(np.argmax(class_probs))
+        return pred
+        
+        
+        
+        
