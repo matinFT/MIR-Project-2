a4c4235368612a30ec41116c7653424946fb4749
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2021-04-29 16:58:02 +0430

    added evaluation metrics

--- src/classification/evaluation.py
+++ src/classification/evaluation.py
@@ -2,23 +2,40 @@ import typing as th
 
 
 def accuracy(y, y_hat) -> float:
-    # todo: for you to implement
-    pass
+    matches = 0
+    for i in range(len(y)):
+        if y[i] == y_hat[i]:
+            matches += 1
+    return matches / len(y)
 
 
 def f1(y, y_hat, alpha: float = 0.5, beta: float = 1.):
-    # todo: for you to implement
-    pass
+    p = precision(y, y_hat)
+    r = recall(y, y_hat)
+    return 2 * p * r / (p + r)
+#     return (beta**2 + 1) * p * r / (b**2 * p + r)
 
 
 def precision(y, y_hat) -> float:
-    # todo: for you to implement
-    pass
+    matches = 0
+    total = 0
+    for i in range(len(y)):
+        if y_hat[i] == 1:
+            total += 1
+            if y[i] == 1:
+                matches += 1
+    return matches / total
 
 
 def recall(y, y_hat) -> float:
-    # todo: for you to implement
-    pass
+    matches = 0
+    total = 0
+    for i in range(len(y)):
+        if y[i] == 1:
+            total += 1
+            if y_hat[i] == 1:
+                matches += 1
+    return matches / total
 
 
 evaluation_functions = dict(accuracy=accuracy, f1=f1, precision=precision, recall=recall)
