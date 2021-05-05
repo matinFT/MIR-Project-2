d28f45d577703d0baf68ee2fe57f9f43989dee52
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2021-05-05 17:28:34 +0430

    added classification evaluation and visualization

--- src/classification/models/knn.py
+++ src/classification/models/knn.py
@@ -1,22 +1,14 @@
 import typing as th
 from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
-import time
 from numpy import linalg as LA
 
 
 class KNN(BaseEstimator, ClassifierMixin):
-    def __init__(
-            self,
-            k = 2,
-            # add required hyper-parameters (if any)
-    ):
+    def __init__(self, k = 2):
         self.k = k
 
-#     def fit(self, x, y, **fit_params):
-
     def fit(self, X, Y):
-        
         self.X = np.array([np.array(x) for x in X])
         self.Y = np.array(Y)
         self.classes_num = max(Y) + 1
@@ -29,9 +21,6 @@ class KNN(BaseEstimator, ClassifierMixin):
             pred.append(np.argmax(np.bincount(nearest_neighbors_classes)))
         return pred
             
-        
     def nearest_neighbors_classes(self, x):
         distances = np.array([LA.norm(a, 2) for a in (self.X - x)])
         return self.Y[distances.argsort()[:self.k]]
-
-                        
